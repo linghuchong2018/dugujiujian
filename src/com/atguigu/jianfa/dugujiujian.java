9a2340f480cf270648d9ba55ea9fdea4f8b6a567
令狐冲 <linghuchong2018@126.com>
2018-04-29 15:40:32 +0800

    update linghuchong

--- src/com/atguigu/jianfa/dugujiujian.java
+++ src/com/atguigu/jianfa/dugujiujian.java
@@ -3,7 +3,7 @@ package com.atguigu.jianfa;
 public class dugujiujian {
 
 	public static void main(String[] args) {
-		System.err.println("ybq");
+		System.err.println("lllllllllllllllllllllllllllllllllll");
 
 	}
 
