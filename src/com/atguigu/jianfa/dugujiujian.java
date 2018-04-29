8812647fd959c4f6fc726fa73ed4097749ce8cff
岳不群 <yuebuqun2018@126.com>
2018-04-29 15:47:40 +0800

    update yuebuqun

--- src/com/atguigu/jianfa/dugujiujian.java
+++ src/com/atguigu/jianfa/dugujiujian.java
@@ -3,7 +3,7 @@ package com.atguigu.jianfa;
 public class dugujiujian {
 
 	public static void main(String[] args) {
-		System.err.println("lllllllllllllllllllllllllllllllllll");
+		System.err.println("yyyyyyyyyyyyyyyyyyyyyyyyyy");
 
 	}
 
